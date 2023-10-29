4cf85f894c30000c71fd9c2c12ae060248797eb9
Ryong <tmdfyd1995@gmail.com>
2023-10-30 01:26:11 +0900

    gitignore 추가

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -4,7 +4,7 @@ import path from "path";
 const router = express.Router();
 
 router.get("/css", (_, res: Response) => {
-  res.sendFile(path.join(__dirname, "../assets/css/oopy.css"));
+  res.sendFile(path.join(__dirname, "assets/css/oopy.css"));
 });
 
 export default router;
