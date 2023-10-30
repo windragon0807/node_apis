0eb7850b81ccef7d69d287b1793bc56551489006
Ryong <tmdfyd1995@gmail.com>
2023-10-31 04:18:48 +0900

    모든 div에 적용으로 변경

--- public/assets/javascript/codeBlock.js
+++ public/assets/javascript/codeBlock.js
@@ -1,4 +1,6 @@
-var parentDiv = document.querySelector('div[class^="CodeBlock_container_"]');
-var newDiv = document.createElement("div");
-parentDiv.appendChild(newDiv);
-newDiv.classList.add("CodeBlock_left");
+var parentDivs = document.querySelectorAll('div[class^="CodeBlock_container_"]'); // 모든 div 엘리먼트 선택
+parentDivs.forEach((parentDiv) => {
+  var newDiv = document.createElement("div");
+  parentDiv.appendChild(newDiv);
+  newDiv.classList.add("CodeBlock_left");
+});
