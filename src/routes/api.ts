10f1a9c81c06098b93ea8fd0ff62f714e3b211ed
Ryong <tmdfyd1995@gmail.com>
2023-10-31 00:53:43 +0900

    css 파일 분리

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,10 +1,21 @@
 import express, { Response } from "express";
 import path from "path";
+import fs from "fs";
 
 const router = express.Router();
 
+const cssFiles = ["callout", "codeBlock", "header", "image", "index", "page"];
+const output = cssFiles
+  .map((fileName) => {
+    const filePath = path.join(process.cwd(), `/public/assets/css/${fileName}.css`);
+    return fs.readFileSync(filePath, "utf-8");
+  })
+  .join("\n");
+
 router.get("/css", (_, res: Response) => {
-  res.sendFile(path.join(process.cwd(), "/public/assets/css/oopy.css"));
+  // res.sendFile(path.join(process.cwd(), "/public/assets/css/index.css"));
+  res.set("Content-Type", "text/css");
+  res.send(output);
 });
 
 export default router;
