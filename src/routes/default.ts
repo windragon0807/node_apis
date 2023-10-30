cbc27f05a37d54150069dedf789533f4c40f6656
Ryong <tmdfyd1995@gmail.com>
2023-10-31 04:24:12 +0900

    불필요한 코드 수정

--- src/routes/default.ts
+++ src/routes/default.ts
@@ -2,21 +2,8 @@ import express, { Response } from "express";
 
 const router = express.Router();
 
-interface Data {
-  id: number;
-  name: string;
-}
-
-const Send: Data = {
-  id: 0,
-  name: "tests",
-};
-
 router.get("/", (_, res: Response) => {
-  res.status(200).json({
-    success: true,
-    data: Send,
-  });
+  res.status(200).send("Hello World");
 });
 
 export default router;
